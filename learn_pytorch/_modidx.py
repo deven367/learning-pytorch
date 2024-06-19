b5aaec4741e28158807de1b0de40f99c6ba5707d
deven367 <masterdeven@gmail.com>
2024-06-19 09:25:35 -0400

    remaining files

--- learn_pytorch/_modidx.py
+++ learn_pytorch/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://deven367.github.io',
                 'git_url': 'https://github.com/deven367/learn-pytorch',
                 'lib_path': 'learn_pytorch'},
-  'syms': {'learn_pytorch.core': {'learn_pytorch.core.foo': ('core.html#foo', 'learn_pytorch/core.py')}}}
+  'syms': { 'learn_pytorch.core': {'learn_pytorch.core.foo': ('core.html#foo', 'learn_pytorch/core.py')},
+            'learn_pytorch.helper': { 'learn_pytorch.helper.accuracy_fn': ( '02-nn-classification.html#accuracy_fn',
+                                                                            'learn_pytorch/helper.py')}}}
